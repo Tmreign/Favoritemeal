bbc32ea64751bd67defe471b1d2f0b22627db9a5
Tmreign <tomwanyoike9@gmail.com>
2019-08-26 11:17:11 +0300

    first commit

---
+++ app/src/main/java/com/tmreign/favoritemeals/DeliciousMeals.java
@@ -0,0 +1,47 @@
+package com.tmreign.favoritemeals;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DeliciousMeals extends AppCompatActivity {
+    @BindView(R.id.listView)ListView mListView;
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
+    private String[] meals = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delicious_meals);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, meals);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String meals = ((TextView)view).getText().toString();
+                Toast.makeText(DeliciousMeals.this, meals, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Place Your Favorite Meals: " + location);
+    }
+}
---
+++ app/src/main/java/com/tmreign/favoritemeals/MainActivity.java
@@ -0,0 +1,42 @@
+package com.tmreign.favoritemeals;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListenergit {
+    @BindView(R.id.mealsButton)
+    Button mealsButton;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mealsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mealsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, DeliciousMeals.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+    }
+
+
+}
