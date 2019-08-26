6c5b67bff88ebf4d693ea9ea9d9b7b1ff272694d
Tmreign <tomwanyoike9@gmail.com>
2019-08-26 11:33:05 +0300

    list of meals

--- app/src/main/java/com/tmreign/favoritemeals/DeliciousMeals.java
+++ app/src/main/java/com/tmreign/favoritemeals/DeliciousMeals.java
@@ -17,7 +17,7 @@ public class DeliciousMeals extends AppCompatActivity {
     @BindView(R.id.listView)ListView mListView;
     @BindView(R.id.locationTextView) TextView mLocationTextView;
     private String[] meals = new String[] {"Beef and Bean Burritos", "Shrimp Scampi",
-            "Fried Round Steak", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Fried Round Steak", "Chicken Florentine Pasta", "Roasted Red Pepper Pasta", "Pepperoni Pizza Burgers",
             "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
             "Lardo", "Portland City Grill", "Fat Head's Brewery",
             "Chipotle", "Subway"};
